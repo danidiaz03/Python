48bc98d6544a8fa13811ed96cf911becc2e4936b
dani <danieldiazq2003@gmail.com>
2024-10-21 15:10:18 +0200

    a

---
+++ Practica2/p2ej2.py
@@ -0,0 +1,40 @@
+mes=int(input("Introduce un numero del 1 al 12: "))
+if mes<1 or mes >12 :
+    print(f'error')
+else:
+    if mes==1:
+        print(f'Enero tiene 31 días')
+    
+    if mes==2:
+        print(f'Febrero tiene 28 días')
+        
+    if mes==3:
+        print(f'Marzo tiene 31 días')
+        
+    if mes==4:
+        print(f'Abril tiene 30 días')
+        
+    if mes==5:
+        print(f'Mayo tiene 31 días')
+        
+    if mes==6:
+        print(f'JUnio tiene 30 días')
+        
+    if mes==7:
+        print(f'JUlio tiene 31 días')
+        
+    if mes==8:
+        print(f'Agosto tiene 31 días')
+    
+    if mes==9:
+        print(f'Septiembre tiene 30 días')
+    
+    if mes==10:
+        print(f'Octubre tiene 31 días')
+        
+    if mes==11:
+        print(f'Noviembre tiene 30 días')
+        
+    if mes==12:
+        print(f'Diciembre tiene 31 días')    
+
