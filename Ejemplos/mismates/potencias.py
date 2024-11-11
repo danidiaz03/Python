636c3b62054660e244c674ca78e99dc62f5b15fa
dani <danieldiazq2003@gmail.com>
2024-11-11 12:20:02 +0100

    q

---
+++ Ejemplos/mismates/potencias.py
@@ -0,0 +1,5 @@
+def cuadrado(x):
+    return x*x
+
+def cubo(x):
+    return x*x*x
---
+++ Ejemplos/mismates/probando.py
@@ -0,0 +1,18 @@
+#import potencias
+
+from operaciones import suma
+
+from potencias import *
+
+import os 
+
+x=3
+y=6
+
+#r=potencias.cubo(x)
+
+res=suma(x,y)
+
+r=cubo(y)
+
+print(os.getcwd())
